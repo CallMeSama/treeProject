82c6a56de4b4e3350c7cb2b73081fac832e0be92
CallMeSama <diarratoulaye07@gmail.com>
2023-07-08 23:49:22 +0000

    Repository class

--- tree/src/repository/Repository.java
+++ tree/src/repository/Repository.java
@@ -2,8 +2,37 @@ package src.repository;
 
 import java.util.ArrayList;
 
+package src.repository;
+
+import java.util.ArrayList;
+
 import src.composite.Composite;
 
 public class Repository extends Composite {
 
+    ArrayList<Composite> children = new ArrayList<Composite>();
+
+    public Repository(String name) {
+        super(name);
+    }
+
+    public void addChild(Composite child) {
+        this.children.add(child);
+    }
+
+    @Override
+    public void show(int tab, int noBranchLevel, int prevNoBranchLevel) {
+
+        tabulate(tab, noBranchLevel, prevNoBranchLevel);
+        System.out.println(getName());
+
+        for (Composite child : children) {
+
+            boolean last = child.equals(children.get(children.size() - 1));
+            if (last)
+                child.show(tab + 1, tab + 1, noBranchLevel);
+            else
+                child.show(tab + 1, noBranchLevel, prevNoBranchLevel);
+        }
+    }
 }
